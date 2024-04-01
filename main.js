daadfefc43cc2b7e7aa463713083f25585173611
Kawan0606 <108882285+Kawan0606@users.noreply.github.com>
2024-04-01 11:08:16 -0300

    save

--- main.js
+++ main.js
@@ -1,39 +1,10 @@
-/*const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll("aba.conteudo");
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
 
         for(let j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-
-        botoes[i].classList.add("ativo");
-    }
-}*/
-/*
-const botoes = document.querySelectorAll(".botao");//aula3
-const textos = document.querySelectorAll(".aba-conteudo");//aula5
-
-for (let i = 0; i < botoes.length; i++) { //aula3
-    botoes[i].onclick = function () { //aula3
-
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");//aula5
-            textos[j].classList.remove("ativo");//aula5
-        } //aula3
-
-        botoes[i].classList.add("ativo");//aula5
-        textos[i].classList.add("ativo");//aula5
-    }
-}*/
-
-const botoes = document.querySelectorAll(".botao");
-const textos = document.querySelectorAll(".aba-conteudo");
-
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
-
-        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -44,7 +15,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-let tempoAtual = new Date();
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+let tempoAtual = nem Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
