ddd7d4ea2ff73c5406d615bfe82ba443e08d5f61
Kawan0606 <108882285+Kawan0606@users.noreply.github.com>
2024-04-03 12:26:24 -0300

    save

--- main.js
+++ main.js
@@ -54,7 +54,3 @@ function calculaTempo(tempoObjetivo) {
     horas %= 24;
     if (tempoFinal > 0){
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    } else {
-        return "Prazo Finalizado";
-    }
-}
