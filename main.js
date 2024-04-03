57a8bd58551de600894a81582052906e39c60c96
Kawan0606 <108882285+Kawan0606@users.noreply.github.com>
2024-04-03 12:34:22 -0300

    save

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ const tempoObjetivo4 = new Date("2024-12-30T00:00:00");
 const tempos =
 [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
-
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
    contadores[i].textContent = calculaTempo(tempos[i]); } 
@@ -51,3 +50,4 @@ function calculaTempo(tempoObjetivo) {
     } else {
         return "Prazo Finalizado";
     }
+}
